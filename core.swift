3a1317b2b71d8428046f3c382b7ce0ec9e8f2724
Kyle Reynolds <kylereynoldsdev@gmail.com>
2024-01-23 01:42:57 -0500

    Add files via upload

---
+++ core.swift
@@ -0,0 +1,34 @@
+// GDALDataset.swift
+class GDALDataset {
+    // Implementation...
+}
+
+// GDALRasterBand.swift
+class GDALRasterBand {
+    // Implementation...
+}
+
+// OGRDataSource.swift
+class OGRDataSource {
+    // Implementation...
+}
+
+// OGRLayer.swift
+class OGRLayer {
+    // Implementation...
+}
+
+// OGRFeature.swift
+class OGRFeature {
+    // Implementation...
+}
+
+// OGRGeometry.swift
+class OGRGeometry {
+    // Implementation...
+}
+
+// OGRSpatialReference.swift
+class OGRSpatialReference {
+    // Implementation...
+}
